be153bad91231dbb3b62e575a71eefc1017dfee3
CFAI-Nicolas <pro.remy.nicolas@gmail.com>
2024-01-01 16:06:49 +0100

    CHG : DBname

--- deploy/bootstrap.php
+++ deploy/bootstrap.php
@@ -6,11 +6,11 @@ require_once "vendor/autoload.php";
 $isDevMode = true;
 $config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
 $conn = array(
-'host' => 'dpg-ccuhk2kgqg4a9295cagg-a',
+'host' => 'dpg-cm23hgfqd2ns73d8du20-a',
 'driver' => 'pdo_pgsql',
-'user' => 'cnam_user',
-'password' => '0TJXmePJA9OkhPb6gllHoVZJIL6aOvDS',
-'dbname' => 'cnam',
+'user' => 'mabdd_user',
+'password' => 'GfH00fj6dSVF80m5zU6G91xqCprd8ONo',
+'dbname' => 'mabdd',
 'port' => '5432'
 );
 $entityManager = EntityManager::create($conn, $config);
